d817c736b619fc4e8690c3330a1a56f22d3e3551
Alexander Vinokurov <vinokurov@sqvr.net>
2020-11-03 02:04:07 +0300

    Change database settings

--- requester/settings.py
+++ requester/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4i92j4-$zvb2^@3@gx9$rus*$1htv^q2&4d#&77^ij1cf6ev!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'requester.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd6ej1lkc4535q1',
-        'USER': 'clkcykxwzpdcyu',
-        'PASSWORD': 'd6d30f029763c870b719e4103dc0b2b7fdb529a56c1b8680331d3f3c13543e2c',
-        'HOST': 'ec2-35-168-77-215.compute-1.amazonaws.com',
+        'NAME': 'def11935kibl1f',
+        'USER': 'vsilrnnhcnpnia',
+        'PASSWORD': 'a8cd567bd20ba2cb86dbf0da19bce941e1519e4e7ae1331f1034c1f5c4fe548c',
+        'HOST': 'ec2-54-146-4-66.compute-1.amazonaws.com',
         'PORT': 5432,
         'CONN_MAX_AGE': 0,
     }
